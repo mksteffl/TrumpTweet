d571ee2be69b965e459c4c6d916914061e4bf593
Mark Steffl <mas529@pitt.edu>
2016-02-20 21:07:09 -0500

    Cleaned up some stuff

--- TrumpTweetTest.py
+++ TrumpTweetTest.py
@@ -1,6 +1,6 @@
 import TweetGenerator
 
-file  = open("Cant Stump The Trump.txt", "rt")
+file  = open("WindowsCorpus.txt", "rt")
 tweetGen = TweetGenerator.TweetGenerator(file)
 
 for i in range(0,10):
